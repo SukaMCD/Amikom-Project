90f027dd1c4d4af5907d327eb7063dbb74c2ef93
SukaMCD <fabian25march@gmail.com>
2026-02-10 12:23:55 +0700

    Implement Author, Book, and Publisher resources with corresponding migrations and views

---
+++ app/Http/Controllers/PublisherController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\publisher;
+use Illuminate\Http\Request;
+
+class PublisherController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(publisher $publisher)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(publisher $publisher)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, publisher $publisher)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(publisher $publisher)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\http\Controllers\AuthorController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+route::resource('author', AuthorController::class);
